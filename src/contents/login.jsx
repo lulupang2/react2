b032df1113319374d3b4c877cc2d75dcd44298fe
lulupang2 <lulupang@live.co.kr>
2021-08-30 03:38:14 +0900

    _

--- src/contents/login.jsx
+++ src/contents/login.jsx
@@ -1,3 +1,4 @@
+//import axios from 'axios'
 import React, { useState } from 'react'
 import '../res/login.css'
 
@@ -14,6 +15,14 @@ const Login = () => {
 
         setPasswd(event.currentTarget.value)
     }
+
+    const onSubmithandler = (event) => {
+        event.preventDefault();
+        console.log('ID = ', Userid);
+        console.log('비번 = ', Passwd);
+
+        //axios.post('../model/login')
+    }
     
 
     return (
@@ -28,12 +37,12 @@ const Login = () => {
         <div className="mainbg">
 <div className="box">
                 <div className="loginbox wrapbox">
-                    <form>
+                    <form onSubmit={onSubmithandler}>
                         <input type="text" name="userid" id="userid" placeholder="아이디를 입력하세요" value={Userid} onChange={onIdhandler} /><br />
                         <input type="password" name="passwd" id="passwd" placeholder="비밀번호를 입력하세요" value={Passwd} onChange={onPasshandler} /><br />
                         <br />
-                        <button id="btn">회원가입</button><br />
-                        <button id="btn">로그인</button>
+                            <button className="btn" type='submit'>로그인</button><br />
+                            <button className="btn" disabled={true}>회원가입</button>
                     </form>
                 </div>
 
